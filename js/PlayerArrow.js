f7a1a7553f0f793f880878b1fd6c0aa875970a5a
aekbote <88759575+aekbote@users.noreply.github.com>
2021-11-25 14:57:55 -0800

    Update PlayerArrow.js

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -27,11 +27,12 @@ class PlayerArrow {
 
    
   }
+  shoot(archerAngle) {
+	var velocity = p5.Vector.fromAngle(archerAngle);
+	velocity.mult(20);
+	Matter.Body.setStatic(this.body, false);
+	Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
+	}
 
-
-  shoot(){
-
-    
-  }
 
 }
